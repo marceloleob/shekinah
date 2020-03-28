de24b6cd7e841573c37b247d7709549ff9f8b0c3
Marcelo Leopold <marceloleob@gmail.com>
2020-03-28 17:19:33 -0400

    Corrigido link do TurnUP Web

--- resources/views/common/botton-footer.blade.php
+++ resources/views/common/botton-footer.blade.php
@@ -7,7 +7,7 @@
                     <span class="copyright_text text-center text_white d-block"><i class="far fa-copyright"></i> Copyright {!! date('Y') !!} - <span class="text_primary">SHEKINAH</span> - Todos os direitos reservados</span>
                 </div>
                 <div class="col-lg-12 col-sm-12 py-1">
-                    <span class="copyright_text text-center text_white d-block">Site criado por <a href="http://www.marceloleodev.com" class="text_white mldev" target="_blank">MarceloLeoDev</a></span>
+                    <span class="copyright_text text-center text_white d-block">Site criado por <a href="https://www.turnupweb.com" class="text_white mldev" target="_blank">TurnUp Web</a></span>
                 </div>
             </div>
         </div>
